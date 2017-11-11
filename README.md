6705dfcd5471a825a3ccf9f748316328381446e5
Maxim Shpilka <pisik8809@gmail.com>
2017-11-11 23:22:51 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dz7 from JS course, UITSchool.
+Author - <a href="https://www.facebook.com/profile.php?id=100005527921768">Maxim Shpilka</a>
---
+++ seven.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", ready);
+function ready() {
+  document.body.addEventListener("click", getTarget);
+ function getTarget(e) {
+    var target = event.target.getBoundingClientRect();
+    console.log(target);
+  }
+  var car = document.getElementsByClassName('car')[0];
+  car.addEventListener('click', carMove);
+  function carMove() {
+   if(event.target.hasAttribute('driver1')) {
+     car.style.position = 'absolute';
+     car.style.left = 200 +'px';
+     console.log(car.style.left);
+   }
+   else if(event.target.hasAttribute('driver2')) {
+     car.style.position = 'absolute';
+     car.style.left = 400 +'px';
+     console.log(car.style.left);
+   }
+ }
+}
