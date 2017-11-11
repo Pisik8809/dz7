a90505464f1c73534948544721e2aea0d6ce30ba
Maxim Shpilka <pisik8809@gmail.com>
2017-11-11 23:32:14 +0200

    second commit

--- seven.js
+++ seven.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", ready);
 function ready() {
-  document.body.addEventListener("click", getTarget);
+ document.body.addEventListener("click", getTarget);
  function getTarget(e) {
     var target = event.target.getBoundingClientRect();
     console.log(target);
@@ -10,12 +10,12 @@ function ready() {
   function carMove() {
    if(event.target.hasAttribute('driver1')) {
      car.style.position = 'absolute';
-     car.style.left = 200 +'px';
+     car.style.left = 400 +'px';
      console.log(car.style.left);
    }
    else if(event.target.hasAttribute('driver2')) {
      car.style.position = 'absolute';
-     car.style.left = 400 +'px';
+     car.style.left = 700 +'px';
      console.log(car.style.left);
    }
  }
